574f1909179b324d75b201eb370756c5ab264490
abdykerimov.urm <urmat.abdykerimov@timelysoft.net>
2021-08-30 11:08:21 +0600

    #3

--- TextToVideo/Services/UserService.cs
+++ TextToVideo/Services/UserService.cs
@@ -4,6 +4,7 @@ using DAL.Entities;
 using Mapster;
 using Microsoft.AspNetCore.Identity;
 using Models.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace Uploader.Services
@@ -20,6 +21,10 @@ namespace Uploader.Services
         public async Task Add(UserModel.AddIn model)
         {
             var user = new User { Email = model.Email, UserName = model.Email };
+            if(await UserManager.FindByNameAsync(model.Email) != null)
+            {
+                throw new Exception($"User with email {model.Email} already exists.");
+            }
             await UserManager.CreateAsync(user, model.Password);
             await UserManager.AddToRoleAsync(user, "user");
         }
